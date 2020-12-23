de8118926965c071ae6fb115c2b2b2ce172f7878
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-12-22 16:45:01 -0800

    Update ig_unfollow.py

--- ig_unfollow.py
+++ ig_unfollow.py
@@ -12,17 +12,24 @@ class InstaBot:
 		home_page = HomePage(self.browser)
 		home_page.login(username, password)
 
-	def unfollow(self, username):
+		self.username = username
+
+	def unfollow(self):
 		# Go to your Instagram profile page
-		self.browser.get("https://www.instagram.com/{}/".format(username))
+		self.browser.get("https://www.instagram.com/{}/".format(self.username))
 		sleep(5)
 
 		# Get the usernames of all your followers
-		followers = self.get_followers()
-		sleep(5)
+		followers, num_of_followers = self.get_followers()
+		
+		# Check to make sure that approximately all followers were scraped
+		# (I say approximately because I've noticed I always have a few less followers than stated on Instagram)
+		if len(followers) < int(num_of_followers) * 0.99:
+			print("There's been an error while scraping the usernames of your followers.")
+			return
 
 		# Unfollow accounts that aren't following you
-		num_of_accounts_unfollowed, accounts_unfollowed = self.compare_followers_to_following(followers)
+		num_of_accounts_unfollowed, accounts_unfollowed = self.compare_to_following_and_unfollow(followers)
 		print("You've unfollowed {} accounts.".format(num_of_accounts_unfollowed))
 		sleep(5)
 		
@@ -32,46 +39,57 @@ class InstaBot:
 		# Store the usernames of accounts you've unfollowed 
 		new_file = open('accounts_unfollowed.txt', 'w')
 		for account_unfollowed in accounts_unfollowed:
-			new_file.write(account_unfollowed)
+			new_file.write(account_unfollowed + ", ")
 		new_file.close()
 
 		return
+
+	def scroll(self, popup_window):
+		js_command = """
+					popup_window = document.querySelector('.isgrP');
+					popup_window.scrollTo(0, popup_window.scrollHeight);
+					var curr_page_len = popup_window.scrollHeight;
+					return curr_page_len;
+					"""
+		curr_page_len = self.browser.execute_script(js_command)
+		scrolling = True
+		sleep(1)
+		while scrolling:
+			prev_page_len = curr_page_len
+			curr_page_len = self.browser.execute_script(js_command)
+			if prev_page_len == curr_page_len:
+				scrolling = False
+			sleep(1)
 
 	def get_followers(self):
 		# Get number of followers
 		num_of_followers = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
 
-		# Click on the "followers" button
-		# this will open a popup window that lists followers
 		followers_button = self.browser.find_element_by_partial_link_text("followers")
 		followers_button.click()
 		sleep(5)
 
 		# Scroll to load entire list of followers
-		scroll_count = 0
-		while scroll_count < 2: #int(num_of_followers) // 2: 
-			scroll_box = self.browser.find_element_by_xpath("//div[@role='dialog']//a")
-			scroll_box.send_keys(Keys.PAGE_DOWN)
-			scroll_count += 1
-			sleep(1)
+		self.scroll(followers_button)
 		print("Done scrolling through followers.")
 
 		# Get the usernames of all followers
-		followers = set()
-		#for i in range(1, num_of_followers + 1):
-		for i in range(3, 0, -1):
-			follower = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[1]/div[2]/div[1]/span/a".format(i))
-			followers.add(follower.get_attribute("title"))
-		sleep(1)
-		
+		usernames_of_followers = set()
+		followers = self.browser.find_elements_by_css_selector('.FPmhX.notranslate')
+		for follower in followers:
+			usernames_of_followers.add(follower.text)
+
 		# Close popup window
 		close_popup_button = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")
 		close_popup_button.click() 
 
-		return followers
+		return usernames_of_followers, num_of_followers
 
-	def compare_followers_to_following(self, followers):
-		# Get number of accounts you were following prior to unfollowing those that don't follow you back
+	def compare_to_following_and_unfollow(self, followers):
+		# Compare followers and following
+		# Unfollow accounts that don't follow you back
+
+		# Get number of accounts you are following (before unfollowing those that don't follow you back)
 		num_following_before = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
 
 		# Click on the "following" button
@@ -81,22 +99,32 @@ class InstaBot:
 		sleep(5)
 
 		# Scroll to load entire list of users you're following 
-		scroll_count = 0
-		while scroll_count < 4: #int(num_of_followers) // 2: 
-			scroll_box = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]")
-			scroll_box.send_keys(Keys.PAGE_DOWN)
-			scroll_count += 1
-			sleep(1)
+		self.scroll(following_button)
 		print("Done scrolling through following.")
 
-		# Unfollow accounts who don't follow you back
-		#for i in range(1, int(num_of_followers) + 1):
+		# Unfollow accounts that don't follow you back
+		accounts_unfollowed = self.unfollow_helper(followers, num_following_before)
+
+		# Close popup window
+		close_popup_button = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")
+		close_popup_button.click() 
+		sleep(1)
+
+		# Get number of accounts you are following now (after ufollowing those that don't follow you back)
+		num_following_after = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
+
+		# Return the number of people you've unfollowed and their usernames
+		num_of_accounts_unfollowed = int(num_following_before) - int(num_following_after)
+		return num_of_accounts_unfollowed, accounts_unfollowed
+
+	def unfollow_helper(self, followers, num_following_before):
 		accounts_unfollowed = set()
-		for i in range(1, 0, -1): 
+
+		for i in range(int(num_following_before), 0, -1):
 			following = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[1]/div[2]/div[1]/span/a".format(i))
-			following_username = following.get_attribute("title")	
-			print(i, following_username)	
+			following_username = following.get_attribute("title")		
 			if following_username not in followers:
+				# Unfollow account
 				following_user_button = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[2]/button".format(i))
 				following_user_button.click()
 				sleep(1)
@@ -104,19 +132,8 @@ class InstaBot:
 				unfollow_user_button.click()
 				sleep(1)
 				accounts_unfollowed.add(following_username)
-		sleep(1)
 
-		# Close popup window
-		close_popup_button = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")
-		close_popup_button.click() 
-		sleep(1)
-
-		# Get number of accounts you are following now
-		num_following_after = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
-
-		# Return the number of people you've unfollowed and their usernames
-		num_of_accounts_unfollowed = int(num_following_before) - int(num_following_after)
-		return num_of_accounts_unfollowed, accounts_unfollowed
+		return accounts_unfollowed
 
 class HomePage:
     def __init__(self, browser):
