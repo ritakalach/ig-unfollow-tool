6ac1b55d1b99e5858940cc894242e8537378ba29
ritakalach <63493018+ritakalach@users.noreply.github.com>
2020-12-30 20:25:55 -0800

    Update ig_unfollow_tool.py

--- ig_unfollow_tool.py
+++ ig_unfollow_tool.py
@@ -24,12 +24,15 @@ class InstaBot:
 		
 		# Check to make sure that approximately all followers were scraped
 		# (I say approximately because I've noticed I always have a few less followers than stated on Instagram)
-		if len(followers) < int(num_of_followers) * 0.99:
+		if len(followers) < num_of_followers * 0.99:
 			print("There's been an error while scraping the usernames of your followers.")
 			return
 
 		# Unfollow accounts that aren't following you
 		num_of_accounts_unfollowed, accounts_unfollowed = self.compare_to_following_and_unfollow(followers)
+		# The number of accounts unfollowed is rounded if you have lots of followers
+		# E.g., if you have 20,426 followers, Instagram will say you have 20.4K followers
+		# and that's why the calculation will be rounded as well
 		print("You've unfollowed {} accounts.".format(num_of_accounts_unfollowed))
 		sleep(5)
 		
@@ -61,9 +64,24 @@ class InstaBot:
 				scrolling = False
 			sleep(1)
 
+	def convert_str_to_num(self, num_as_str):
+		num_map = {'K':1000, 'M':1000000, 'B':1000000000}
+		num_as_str = num_as_str.replace(",", "")
+
+		last_ch = num_as_str[-1]
+		if last_ch in num_map:
+			num_as_int = float(num_as_str[:-1])
+			num_as_int *= num_map[last_ch]
+			num_as_int = int(num_as_int)
+		else:
+			num_as_int = int(num_as_str)
+
+		return num_as_int
+
 	def get_followers(self):
 		# Get number of followers
 		num_of_followers = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a/span").text
+		num_of_followers = self.convert_str_to_num(num_of_followers)
 
 		followers_button = self.browser.find_element_by_partial_link_text("followers")
 		followers_button.click()
@@ -91,6 +109,7 @@ class InstaBot:
 
 		# Get number of accounts you are following (before unfollowing those that don't follow you back)
 		num_following_before = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
+		num_following_before = self.convert_str_to_num(num_following_before)
 
 		# Click on the "following" button
 		# this will open a popup window that lists accounts you're following
@@ -112,16 +131,27 @@ class InstaBot:
 
 		# Get number of accounts you are following now (after ufollowing those that don't follow you back)
 		num_following_after = self.browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a/span").text
+		num_following_after = self.convert_str_to_num(num_following_after)
 
 		# Return the number of people you've unfollowed and their usernames
-		num_of_accounts_unfollowed = int(num_following_before) - int(num_following_after)
+		num_of_accounts_unfollowed = num_following_before - num_following_after
 		return num_of_accounts_unfollowed, accounts_unfollowed
 
 	def unfollow_helper(self, followers, num_following_before):
 		accounts_unfollowed = set()
 
 		for i in range(int(num_following_before), 0, -1):
-			following = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[1]/div[2]/div[1]/span/a".format(i))
+			try:
+				following = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[1]/div[2]/div[1]/span/a".format(i))
+			except:
+				# Sometimes information about a user will popup, obscuring the unfollow button
+				# In this case, move the cursor and the popup will disappear
+				action = ActionChains(self.browser) 
+				action.move_by_offset(1000, 1000) 
+				action.perform() 
+				sleep(5)
+				following = self.browser.find_element_by_xpath("/html/body/div[5]/div/div/div[2]/ul/div/li[{}]/div/div[1]/div[2]/div[1]/span/a".format(i))
+
 			following_username = following.get_attribute("title")		
 			if following_username not in followers:
 				# Unfollow account
@@ -132,6 +162,7 @@ class InstaBot:
 				unfollow_user_button.click()
 				sleep(1)
 				accounts_unfollowed.add(following_username)
+				print("You've unfollowed {}.".format(following_username))
 
 		return accounts_unfollowed
 
